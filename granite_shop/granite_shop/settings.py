c239482b23aac5ba91b3f8c59d8a541c1715c6c9
Oleh <musiychuk09@gmail.com>
2025-04-19 13:01:21 +0300

    Create .env.sample and change SECRET_KEY in settings.py

--- granite_shop/granite_shop/settings.py
+++ granite_shop/granite_shop/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d%80(7f-=j#n-k6vu17ax)e_6)6n*iti!*v(495rzk#zj=no9e"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
